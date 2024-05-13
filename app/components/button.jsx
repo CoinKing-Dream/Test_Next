f0343469ef1902258fa877a42a2168fa2ebaf11e
MitsuiMinoru <mitsuimino711@gamil.com>
2024-05-13 23:25:02 +1000

    Test Project...

---
+++ app/components/button.jsx
@@ -0,0 +1,17 @@
+const PinkButton = ({ text, isPurple, onClick, responsive }) => {
+    return (
+      <button
+        type="button"
+        onClick={onClick}
+        className= { 
+             "px-7 py-2 m-3 text-xl rounded-3xl " + responsive + " " + 
+            (isPurple ? " bg-purple-600  text-white" : "border-solid border-2 border-fuchsia-600 text-black") 
+        }
+        // "":} text-2xl`
+      >
+        {text}
+      </button>
+    );
+  };
+  
+  export default PinkButton;
